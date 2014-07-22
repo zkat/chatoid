05389ab7d7b205d41161bea0fa897eff94fddfb2
Kat Marchán <kzm@sykosomatic.org>
2014-07-21 22:14:43 -0700

    peer-call element

---
+++ elements/peer-call/index.js
@@ -0,0 +1,51 @@
+module Polymer from "polymer";
+
+Polymer("peer-call", {
+  publish: {
+    sourcePeer: null,
+    sourceStream: null,
+    peerId: "",
+    peerStream: null,
+    peerStreamUrl: "",
+    /* Danger. Don't use this unless you know what you're doing. */
+    mediaConnection: null
+  },
+  streamReady: function(stream) {
+    this.peerStreamUrl = window.URL.createObjectURL(stream);
+    this.peerStream = stream;
+    this.fire("stream", stream);
+  },
+  mediaConnectionChanged: function() {
+    this.mediaConnection.on("stream", this.streamReady.bind(this));
+    this.mediaConnection.on("error", (function(e) {
+      this.fire("error", e);
+    }).bind(this));
+    this.mediaConnection.on("close", (function() {
+      this.fire("close");
+    }).bind(this));
+  },
+  call: function(force) {
+    if (this.mediaConnection && !force) {
+      this.fire("error", new Error("Call already established"));
+    } else if (this.sourcePeer && this.sourceStream && this.peerId) {
+      console.log("Calling peer: ", this.peerId);
+      this.mediaConnection = this.sourcePeer.call(this.peerId, this.sourceStream);
+    } else {
+      this.fire("error",
+                new Error("source-peer, source-stream, and peer-id are required."));
+    }
+  },
+  close: function() {
+    if (this.mediaConnection) {
+      this.mediaConnection.close();
+      delete this.mediaConnection;
+    }
+  },
+  ready: function() {
+    this.call();
+  },
+  detached: function() {
+    this.close();
+  }
+});
+
