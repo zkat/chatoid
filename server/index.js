1f1f5a8382204c3eb4321b881423f1ef0222abc8
Kat Marchán <kzm@sykosomatic.org>
2014-07-22 21:54:37 -0700

    PeerJS Server support (kinda, doesn't work)

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ var join = require("path").join;
 
 var staticDir = join(__dirname, "/../dist");
 
+var PeerServer = require("peer").PeerServer;
+
 var web = clone(webServer.WebServer, {
   sessionSecret: "omgsupersecretlol",
   staticDir: staticDir
@@ -34,4 +36,11 @@ clone(require("./socket-server").SocketServer, web.http, {
 
 module.exports = function(port) {
   webServer.listen(web, port || 8080);
+  var peerServer = new PeerServer(config.app.peerServerOpts);
+  peerServer.on("connection", function(id) {
+    console.log("Peer connected: ", id);
+  });
+  peerServer.on("disconnect", function(id) {
+    console.log("Peer disconnected: ", id);
+  });
 };
