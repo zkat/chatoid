67b4014c0d2647d8f2b326dca9695477fd9d9eed
Josh Marchán <sykopomp@sykosomatic.org>
2014-07-22 20:38:07 -0700

    Room service

---
+++ server/services/room/resources/room.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var socketServer = require("../../../socket-server"),
+    reply = socketServer.reply,
+    send = socketServer.send,
+    reject = socketServer.reject;
+
+var _ = require("lodash");
+
+function serializeRoom(room) {
+  return {
+    key: room.key,
+    name: room.name,
+    description: room.description,
+    logo: room.logo,
+    users: _.map(room.users, function(user) { return _.pick(user, ["name", "key", "peerId"]); }),
+    userCount: (room.users||[]).length
+  };
+}
+
+var MAX_ROOM_NAME_LENGTH = 200;
+
+module.exports = {
+  join: function(roomService, data, req) {
+    var user = req.from,
+        room;
+    if (data.key.length > MAX_ROOM_NAME_LENGTH) {
+      return reject(req, {msg: "Room name too long"});
+    }
+    if (!roomService.rooms[data.key]) {
+      roomService.rooms[data.key] = {
+        key: data.key,
+        name: data.key,
+        description: "",
+        logo: "",
+        maxId: 0,
+        users: [],
+        bans: []
+      };
+    }
+    room = roomService.rooms[data.key];
+    if (_.contains(room, req.from.fingerprint)) {
+      return reject(req, {msg: "You are banned."});
+    }
+    req.from.name = "boringname-"+(++room.maxId);
+    req.from.peerId = data.peerId;
+    req.from.room = room;
+    room.users.push(req.from);
+    _.forEach(room.users, function(user) {
+      send(user, {type: "join", data: _.pick(req.from, ["name", "key", "peerId"])}, "room");
+    });
+    req.from.on("close", function() {
+      if (req.from.room === room) {
+        room.users = _.without(room.users, req.from);
+        _.forEach(room.users, function(user) {
+          send(user, {type: "part", data: _.pick(req.from, ["name", "key", "peerId"])}, "room");
+        });
+      }
+    });
+    return reply(req, {
+      room: serializeRoom(room),
+      userName: req.from.name
+    });
+  },
+  list: function(roomService, data, req) {
+    reply(req, _.map(roomService.rooms, serializeRoom));
+  }
+};
