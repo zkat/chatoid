d389884bcf10697b6e1d3977aad5dcc1c145efef
Kat Marchán <kzm@sykosomatic.org>
2014-07-29 01:30:00 -0700

    Resolving some cross-browser issues

--- elements/room-user/index.js
+++ elements/room-user/index.js
@@ -23,37 +23,22 @@ Polymer("room-user", {
     }
   },
   userStreamChanged: function() {
-    // TODO - handles tracks being added/removed
-    var stream = this.user.originalStream || this.user.stream;
-    if (stream) {
-      var user = this.user;
-      [].forEach.call(stream.getVideoTracks(), function(track) {
-        var checker = () => user.videoAvailable = track.readyState === "live";
-        checker();
-        Object.observe(track, (changes) => console.log(changes));
-      });
-      [].forEach.call(stream.getAudioTracks(), function(track) {
-        var checker = () => user.audioAvailable = track.readyState === "live";
-        checker();
-        track.addEventListener("started", checker);
-        track.addEventListener("muted", checker);
-        track.addEventListener("unmuted", checker);
-      });
-    }
+    this.userVideoChanged();
+    this.userAudioChanged();
   },
   userVideoChanged: function() {
     this.ensureMediaFlags();
-    var stream = this.user.originalStream || this.user.stream;
-    if (stream) {
-      [].forEach.call(stream.getVideoTracks(),
+    var user = this.user;
+    if (user && user.stream) {
+      [].forEach.call(user.stream.getVideoTracks(),
                       (t) => t.enabled = this.user.video);
     }
   },
   userAudioChanged: function() {
     this.ensureMediaFlags();
-    var stream = this.user.originalStream || this.user.stream;
-    if (stream) {
-      [].forEach.call(stream.getAudioTracks(),
+    var user = this.user;
+    if (user && user.stream) {
+      [].forEach.call(user.stream.getAudioTracks(),
                       (t) => t.enabled = this.user.audio);
     }
   }
